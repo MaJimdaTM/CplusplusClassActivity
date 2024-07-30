0997c8812245b8f6048e66f98620388a6cd01fa8
MaJimdaTM <104894191+MaJimdaTM@users.noreply.github.com>
2024-07-30 19:18:23 +0000

    Pushed page 1 of C++ Chunks by MaJimda

---
+++ "Using the increment and decrement operators.cpp
@@ -0,0 +1,40 @@
+//Activity 1: Using the increment operator '++'
+
+#include<iostream>
+using namespace std;
+
+int main ()
+{   int a;
+    char b;
+    a=0, b=0;
+    cout<<"\n type-in an integer \t";
+    cin>>a; cout<<a; cout<<"\t";
+    a++; cout<<a; cout<<"\t";
+    a++; cout<<a; cout<<"\t";
+    cout<<"\n type in a letter \t";
+    cin>>b; cout<<b; cout<<"\t";
+    b++; cout<<b; cout<<"\t";
+    b++; cout<<b; cout<<"\t";
+    return 0;
+}
+
+//Activity 1: Using the decrement operator '++'
+
+#include<iostream>
+using namespace std;
+
+int main ()
+
+{   int a;
+    char b;
+    a=0, b=0;
+    cout<<"\n type-in an integer \t";
+    cin>>a; cout<<a; cout<<"\t";
+    a--; cout<<a; cout<<"\t";
+    a--; cout<<a; cout<<"\t";
+    cout<<"\n type in a letter \t";
+    cin>>b; cout<<b; cout<<"\t";
+    b--; cout<<b; cout<<"\t";
+    b--; cout<<b; cout<<"\t";
+    return 0;
+}
